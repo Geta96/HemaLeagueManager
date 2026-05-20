c6351ab124eb7b6071349f98e42624f28b790198
Tamás Gergely <tamas.gergely@bbraun.com>
2026-05-20 09:25:17 +0200

    Execuatable builder added

---
+++ build.bat
@@ -0,0 +1,8 @@
+@echo off
+dotnet publish -c Release -r win-x64 --self-contained true ^
+    -p:PublishSingleFile=true ^
+    -p:IncludeNativeLibrariesForSelfExtract=true ^
+    -o publish
+echo.
+echo Built: %cd%\publish\HemaLeagueManager.exe
+pause
